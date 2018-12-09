f1310bece8d2a45d5959479e10b448ec6432a677
SamEmb <40595362+SamEmb@users.noreply.github.com>
2018-12-10 00:18:15 +0530

    Update DC_motor_PID_control.ino

--- DC_motor_PID_control.ino
+++ DC_motor_PID_control.ino
@@ -1,13 +1,4 @@
-/*double count = 0; //set the counts of the encoder
-#double angle = 0;//set the angles
-#boolean A,B;
-#byte state, statep;*/
-// this is the PWM pin for the motor for how much we move it to correct for its error
-
 int dir = 2 ;//these pins are to control the direction of the motor (clockwise/counter-clockwise)
-
-
-//double setpoint = 100;//I am setting it to move through 100 degrees
 double Kp = 1 ;// you can set these constants however you like depending on trial & error
 double Ki = 0;
 double Kd = 0;
@@ -30,13 +21,8 @@ int pwm = 3;
 
 void setup() {
   Serial.begin(9600);
-  //pinMode(2, INPUT);//encoder pins
   pinMode(pwm, OUTPUT);
-  /*attachInterrupt(0,Achange,CHANGE);//interrupt pins for encoder
-  attachInterrupt(1,Bchange,CHANGE); 
-  */
   pinMode(A0,INPUT);
-  //pinMode(dir1, OUTPUT);
   pinMode(dir, OUTPUT);
 
 }
@@ -48,6 +34,10 @@ void loop(){
     setpoint = Serial.parseInt();     
   }
   Serial.println(setpoint);  */
+  int pin1 = analogRead(A0);  
+  int angle ;
+  angle = map(pin1,0,1023,0,360); //count to angle conversion  
+  Serial.println(angle);  
   PIDcalculation();// find PID value  
   
   if (angle < setpoint) {
@@ -69,10 +59,7 @@ void loop(){
 }
 
 void PIDcalculation(){
-  int pin1 = analogRead(A0);  
-  int angle ;
-  angle = map(pin1,0,1023,0,360); //count to angle conversion  
-  Serial.println(angle);  
+ 
   error = setpoint - angle;  
   changeError = error - last_error; // derivative term
   totalError += error; //accumalate errors to find integral term
@@ -82,82 +69,4 @@ void PIDcalculation(){
   last_error = error;
 }
   
-/*void Achange() //these functions are for finding the encoder counts
-{
-  A = digitalRead(2);
-  B = digitalRead(3);
-
-  if ((A==HIGH)&&(B==HIGH)) state = 1;
-  if ((A==HIGH)&&(B==LOW)) state = 2;
-  if ((A==LOW)&&(B==LOW)) state = 3;
-  if((A==LOW)&&(B==HIGH)) state = 4;
-  switch (state)
-  {
-    case 1:
-    {
-      if (statep == 2) count++;
-      if (statep == 4) count--;
-      break;
-    }
-    case 2:
-    {
-      if (statep == 1) count--;
-      if (statep == 3) count++;
-      break;
-    }
-    case 3:
-    {
-      if (statep == 2) count --;
-      if (statep == 4) count ++;
-      break;
-    }
-    default:
-    {
-      if (statep == 1) count++;
-      if (statep == 3) count--;
-    }
-  }
-  statep = state;
-
-}
-
-void Bchange()
-{
-  A = digitalRead(2);
-  B = digitalRead(3);
-
-  if ((A==HIGH)&&(B==HIGH)) state = 1;
-  if ((A==HIGH)&&(B==LOW)) state = 2;
-  if ((A==LOW)&&(B==LOW)) state = 3;
-  if((A==LOW)&&(B==HIGH)) state = 4;
-  switch (state)
-  {
-    case 1:
-    {
-      if (statep == 2) count++;
-      if (statep == 4) count--;
-      break;
-    }
-    case 2:
-    {
-      if (statep == 1) count--;
-      if (statep == 3) count++;
-      break;
-    }
-    case 3:
-    {
-      if (statep == 2) count --;
-      if (statep == 4) count ++;
-      break;
-    }
-    default:
-    {
-      if (statep == 1) count++;
-      if (statep == 3) count--;
-    }
-  }
-  statep = state;
- */ 
-
-
 
